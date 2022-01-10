2104882bbbcd906b926c464e14d9c100e0bcb8c5
Amitkumar Lad <96560166+amitkumardlad@users.noreply.github.com>
2022-01-10 12:14:31 +0530

    Update checkLog4j.java
    Fix issue for Log4j2 issue

--- src/test/java/com/automation/develop/practice/checkLog4j.java
+++ src/test/java/com/automation/develop/practice/checkLog4j.java
@@ -8,9 +8,6 @@ import org.apache.logging.log4j.Logger;
 public class checkLog4j {
     public static Logger logger = LogManager.getLogger(checkLog4j.class.getName());
     public static void main(String[] args) {
-        System.setProperty("log4j.configurationFile", "./resources/properties/log4j2.properties");
-
-        // logger = LogManager.getLogger(this);  //(BaseClass.class)
         logger.debug("This is a debug message");
         logger.info("This is an info message");
         logger.warn("This is a warn message");
